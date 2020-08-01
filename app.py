ae460b738d122e4cde1a06c2a3d6f64744ee46ee
umesh417 <31684921+umesh417@users.noreply.github.com>
2020-08-02 00:57:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask('hello-cloudbuild')
+app = Flask('hello-world')
 
 @app.route('/')
 def hello():
   return "Hello World!\n"
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 8080)
+  app.run(host = '0.0.0.0', port = 3000)
